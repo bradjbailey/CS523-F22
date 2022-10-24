b6e34a4ac59d5d35c4725fcd0e1503d9ac3dceaf
Brad Bailey <59614652+bradjbailey@users.noreply.github.com>
2022-10-24 09:11:44 -0700

    import KNNImputer

--- library.py
+++ library.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 import warnings
+from sklearn.impute import KNNImputer
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------
 
